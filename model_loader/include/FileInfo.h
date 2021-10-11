09ddbc6de3a7eb738910aba2dde2d5dcdc46114b
s1703955 <s1703955@glos.ac.uk>
2021-10-11 19:23:38 +0100

    updated fileinfo.h
    forgot to commit this one oops

--- model_loader/include/FileInfo.h
+++ model_loader/include/FileInfo.h
@@ -2,7 +2,11 @@
 struct FileInfo
 {
 public:
+<<<<<<< Updated upstream
 	std::string path = 0;
+=======
+	std::string path = "";
+>>>>>>> Stashed changes
 	std::streamsize size = 0;
 	long processTime = 0;
 	char* pszData = 0;
