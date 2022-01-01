b941e243bcd83c5b7e73d7e59d47517a03e2a058
s1703955 <s1703955@glos.ac.uk>
2022-01-01 19:52:32 +0000

    Revert "Update Event.h"
    This reverts commit dfb66eef44048a63c4a72f456f1e9277dca27390.

--- model_loader/include/Event.h
+++ model_loader/include/Event.h
@@ -2,28 +2,12 @@
 
 class Event 
 {
-public:
 	Event() : m_bHandled(false) {};
 	virtual ~Event();
-	using DescriptorType = const char*;
-	virtual DescriptorType type() const = 0;
+	using DesciptorType = const char*;
+	virtual DesciptorType type() const = 0;
 	void Handled() { m_bHandled = true; };
 	bool IsHandled() { return m_bHandled; };
 private:
 	bool m_bHandled;
 };
-
-//event overloads
-class WindowResizeEvent : public Event
-{
-public:
-	virtual ~WindowResizeEvent() {};
-	WindowResizeEvent(unsigned int a_width, unsigned int a_height) : m_width(a_width), m_height(a_height) {}
-	static constexpr DescriptorType descriptor = "WindowResizeEvent"; //should this be in header file? Jamie makes a dispatcher.cpp for 1 line of code because we dont want to initialize inside header but we initialize this here? :S
-	virtual DescriptorType Type() const { return descriptor; };
-	inline unsigned int GetWidth() { return m_width; }
-	inline unsigned int GetHeight() { return m_height; }
-private:
-	unsigned int m_width;
-	unsigned int m_height;
-}
